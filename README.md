45918c9bade039d0941913ffcb9bee394881a400
Gerzer <gjacobycooper_16@speyerlegacyschool.org>
2014-11-09 10:07:54 -0500

    Alpha 1.0
    Finally, support for basic PHP scripts! This opens up a whole new realm
    for advanced integration with other technologies. Servr - desktop
    edition PRO automatically includes any output from PHP scripts via
    `echo` in the HTML served.

--- README.md
+++ README.md
@@ -1,12 +1,22 @@
 Servr - desktop edition PRO
 =====
-  
-|||||   ***COMING SOON***   |||||  
-  
-**Start by following the instructions at the [Servr homepage](http://gerzer.github.io/Servr).**  
-A simple HTTP server in Python.  
-1. Download "Servr - desktop edition.py".  
-2. Create a plain text file called "Config.txt" in the same folder as the Python script you downloaded in step 1.  
-3. Modify the file to your needs (see wiki for more info).  
-  
+
+## A simple HTTP server in Python.  
+***Start by following the instructions at the [Servr homepage](http://gerzer.github.io/Servr).***  
+### Requirements  
+- Python 2.6 or higher
+- A relatively modern version of PHP
+- An Internet connection
+- Admin privilages  
+
+### Installation  
+1. Download "Servr - desktop edition PRO.py"  
+2. Download [php.py](https://github.com/brool/util/blob/master/php.py) to the same folder as the Python script you downloaded in step 1  
+3. Download [simplejson](https://pypi.python.org/pypi/simplejson/)  
+4. Extract the archive downloaded in step 3
+5. In your system's command shell, change directory into the extracted simplejson folder
+6. In your system's command shell with the current directory being inside the simplejson folder, run the command `python setup.py install`  
+7. Create a plain text file called "Config.txt" in the same folder as the Python script you downloaded in step 1  
+8. Modify the file to your needs (see wiki for more info)  
+
 Also, check out Servr - mobile edition on the iOS App Store for iPhone and iPod touch!
--- Servr - desktop edition.py
+++ Servr - desktop edition.py
@@ -1,6 +1,9 @@
 from wsgiref.simple_server import make_server
 import os
 import mimetypes
+import subprocess
+import php
+import re
 print "Welcome to Servr - desktop edition!"
 autoStartConfigFile = open("Config.txt", "a+")
 autoStartConfigFile.close()
@@ -11,50 +14,132 @@ data = []
 mimeTypes = []
 fileNames = []
 if doAutoStart == "n":
-  fileName = raw_input("Enter homepage HTML file name including extension:")
-  dataFile = open("Resources/" + fileName, "r")
-  htmlData = dataFile.read().split("\n")
-  dataFile.close()
-  resourceFileNameList = os.listdir("Resources")
-  for fileName in resourceFileNameList:
-    dataFile = open("Resources/" + fileName, "r")
-    mimeTypes.append(mimetypes.guess_type(fileName))
-    data.append(dataFile.read())
-    resourceList = fileName.split("/")
-    fileNames.append(resourceList[len(resourceList) - 1])
-    dataFile.close()
-  address = raw_input("Enter this device's private IP address:")
-  port = raw_input("Enter an unused port:")
+	fileName = raw_input("Enter homepage HTML file name including extension:")
+	dataFile = open("Resources/" + fileName, "r")
+	htmlData = dataFile.read().split("\n")
+	dataFile.close()
+	resourceFileNameList = os.listdir("Resources")
+	os.chdir("Resources")
+	for fileName1 in resourceFileNameList:
+		if os.path.isdir(fileName1):
+			resourceFileNameList2 = os.listdir(fileName1)
+			os.chdir(fileName1)
+			for fileName2 in resourceFileNameList2:
+				if os.path.isdir(fileName2):
+					resourceFileNameList3 = os.listdir(fileName2)
+					os.chdir(fileName2)
+					for fileName3 in resourceFileNameList3:
+						if not os.path.isdir(fileName3) and fileName3 != ".DS_Store":
+							dataFile = open(fileName3, "r")
+							mimeTypes.append(mimetypes.guess_type(fileName3))
+							data.append(dataFile.read())
+							resourceList = fileName3.split("/")
+							del resourceList[0]
+							fileNames.append(fileName1 + "/" + fileName2 + "/" + fileName3)
+							dataFile.close()
+					os.chdir("..")
+				else:
+					if fileName2 != ".DS_Store":
+						dataFile = open(fileName2, "r")
+						mimeTypes.append(mimetypes.guess_type(fileName2))
+						data.append(dataFile.read())
+						resourceList = fileName2.split("/")
+						del resourceList[0]
+						fileNames.append(fileName1 + "/" + fileName2)
+						dataFile.close()
+			os.chdir("..")
+		else:
+			if fileName1 != ".DS_Store":
+				dataFile = open(fileName1, "r")
+				mimeTypes.append(mimetypes.guess_type(fileName))
+				data.append(dataFile.read())
+				resourceList = fileName1.split("/")
+				fileNames.append(resourceList[len(resourceList) - 1])
+				dataFile.close()
+	os.chdir("..")
+	address = raw_input("Enter this device's private IP address:")
+	port = raw_input("Enter an unused port:")
 elif doAutoStart == "y":
-  print "Getting data from Config.txt..."
-  fileName = autoStartConfig[1]
-  resources = os.listdir("Resources")
-  for resource in resources:
-    dataFile = open("Resources/" + resource, "r")
-    mimeTypes.append(mimetypes.guess_type(resource))
-    data.append(dataFile.read())
-    resourceList = resource.split("/")
-    fileNames.append(resourceList[len(resourceList) - 1])
-    dataFile.close()
-  dataFile = open("Resources/" + fileName, "r")
-  htmlData = dataFile.read().split("\n")
-  address = autoStartConfig[2]
-  port = autoStartConfig[3]
+	print "Getting data from Config.txt..."
+	fileName = autoStartConfig[1]
+	resourceFileNameList = os.listdir("Resources")
+	os.chdir("Resources")
+	for fileName1 in resourceFileNameList:
+		if os.path.isdir(fileName1):
+			resourceFileNameList2 = os.listdir(fileName1)
+			os.chdir(fileName1)
+			for fileName2 in resourceFileNameList2:
+				if os.path.isdir(fileName2):
+					resourceFileNameList3 = os.listdir(fileName2)
+					os.chdir(fileName2)
+					for fileName3 in resourceFileNameList3:
+						if not os.path.isdir(fileName3) and fileName3 != ".DS_Store":
+							dataFile = open(fileName3, "r")
+							mimeTypes.append(mimetypes.guess_type(fileName3))
+							data.append(dataFile.read())
+							resourceList = fileName3.split("/")
+							del resourceList[0]
+							fileNames.append(fileName1 + "/" + fileName2 + "/" + fileName3)
+							dataFile.close()
+					os.chdir("..")
+				else:
+					if fileName2 != ".DS_Store":
+						dataFile = open(fileName2, "r")
+						mimeTypes.append(mimetypes.guess_type(fileName2))
+						data.append(dataFile.read())
+						resourceList = fileName2.split("/")
+						del resourceList[0]
+						fileNames.append(fileName1 + "/" + fileName2)
+						dataFile.close()
+			os.chdir("..")
+		else:
+			if fileName1 != ".DS_Store":
+				dataFile = open(fileName1, "r")
+				mimeTypes.append(mimetypes.guess_type(fileName))
+				data.append(dataFile.read())
+				resourceList = fileName1.split("/")
+				fileNames.append(resourceList[len(resourceList) - 1])
+				dataFile.close()
+	os.chdir("..")
+	dataFile = open("Resources/" + fileName, "r")
+	htmlData = dataFile.read().split("\n")
+	address = autoStartConfig[2]
+	port = autoStartConfig[3]
 def host(environ, start_response):
-  i = 0
-  if environ["PATH_INFO"] == None or environ["PATH_INFO"] == "/home" or environ["PATH_INFO"] == "/index.html" or environ["PATH_INFO"] == "/":
-    dataToReturn = str("".join(htmlData))
-    mimeType = "text/html"
-  else:
-    for resource in data:
-      if environ["PATH_INFO"] == "/" + str(fileNames[i]):
-        mimeType = str(mimeTypes[i][0])
-        dataToReturn = resource
-      i += 1
-  status = "200 OK"
-  headers = [("Content-type", mimeType)]
-  start_response(status, headers)
-  return [dataToReturn]
+	i = 0
+	if environ["PATH_INFO"] == None or environ["PATH_INFO"] == "/home" or environ["PATH_INFO"] == "/index.html" or environ["PATH_INFO"] == "/":
+		dataToReturn = str("".join(htmlData))
+		mimeType = "text/html"
+	else:
+		for resource in data:
+			if environ["PATH_INFO"] == "/" + str(fileNames[i]):
+				if ".php" in str(fileNames[i]):
+					dataToReturn = ""
+					resource = resource.split("</body>")[0]
+					n = 0
+					phpStartList = [m.start() for m in re.finditer(r"\<\?php", resource)]
+					phpEndList = [m.start() for m in re.finditer(r"\?\>", resource)]
+					for inst in phpStartList:
+						phpStart = resource.find("<?php") + 5
+						phpEnd = resource.find("?>")
+						phpCode = resource[phpStart:phpEnd]
+						resource2 = resource[:resource.find("<?php")] + resource[resource.find("?>") + 2:]
+						phpResponse = php.PHP().get_raw(phpCode)
+						resource = resource2 + phpResponse
+						n += 1
+						dataToReturn = resource + "</body></html>"
+						mimeType = "text/html"
+					else:
+						mimeType = str(mimeTypes[i][0])
+						dataToReturn = resource
+			i += 1
+	status = "200 OK"
+	try:
+		headers = [("Content-type", mimeType)]
+	except:
+		headers = []
+	start_response(status, headers)
+	return [dataToReturn]
 webServer = make_server(address, int(port), host)
 i = 0
 servers = []
