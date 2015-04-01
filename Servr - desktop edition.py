3ad3206184e3e3be31ab996a6acc5eea6d4ddcaa
cclauss <cclauss@bluewin.ch>
2015-04-01 19:26:20 +0200

    Consolidate fileNames_mimeTypes_and_data() logic
    Use `with open()` to ensure files are closed even when exceptions are thrown.

--- Servr - desktop edition.py
+++ Servr - desktop edition.py
@@ -2,44 +2,35 @@ from wsgiref.simple_server import make_server
 import os
 import mimetypes
 print "Welcome to Servr - desktop edition!"
-autoStartConfigFile = open("Config.txt", "a+")
-autoStartConfigFile.close()
-autoStartConfigFile = open("Config.txt", "r")
-autoStartConfig = autoStartConfigFile.read().split("\n")
-doAutoStart = autoStartConfig[0]
-data = []
-mimeTypes = []
-fileNames = []
+with open("Config.txt", "a+") as autoStartConfigFile:
+  pass
+with open("Config.txt", "r") as autoStartConfigFile:
+  autoStartConfig = autoStartConfigFile.read().split("\n")
+doAutoStart = autoStartConfig[0].lower()
 if doAutoStart == "n":
   fileName = raw_input("Enter homepage HTML file name including extension:").strip()
-  dataFile = open("Resources/" + fileName, "r")
-  htmlData = dataFile.read().split("\n")
-  dataFile.close()
-  resourceFileNameList = os.listdir("Resources")
-  for fileName in resourceFileNameList:
-    dataFile = open("Resources/" + fileName, "r")
-    mimeTypes.append(mimetypes.guess_type(fileName))
-    data.append(dataFile.read())
-    resourceList = fileName.split("/")
-    fileNames.append(resourceList[len(resourceList) - 1])
-    dataFile.close()
   address = raw_input("Enter this device's private IP address:").strip()
   port = raw_input("Enter an unused port:").strip()
 elif doAutoStart == "y":
   print "Getting data from Config.txt..."
-  fileName = autoStartConfig[1]
-  resources = os.listdir("Resources")
-  for resource in resources:
-    dataFile = open("Resources/" + resource, "r")
-    mimeTypes.append(mimetypes.guess_type(resource))
-    data.append(dataFile.read())
-    resourceList = resource.split("/")
-    fileNames.append(resourceList[len(resourceList) - 1])
-    dataFile.close()
-  dataFile = open("Resources/" + fileName, "r")
+  fileName, address, port = autoStartConfig[1:3]
+with open("Resources/" + fileName, "r") as dataFile:
   htmlData = dataFile.read().split("\n")
-  address = autoStartConfig[2]
-  port = autoStartConfig[3]
+
+def fileNames_mimeTypes_and_data(directory="Resources"):
+  data = []
+  mimeTypes = []
+  fileNames = []
+  for fileName in os.listdir(directory):
+    with open(directory + "/" + fileName, "r") as dataFile:
+      mimeTypes.append(mimetypes.guess_type(fileName))
+      data.append(dataFile.read())
+      resourceList = fileName.split("/")
+      fileNames.append(resourceList[-1])
+  return fileNames, mimeTypes, data
+
+fileNames, mimeTypes, data = fileNames_mimeTypes_and_data()
+
 def host(environ, start_response):
   i = 0
   if environ["PATH_INFO"] in (None, "/", "/home", "/index.html"):
